31587d0799a0dd14436d139a934e5b06e4c8deeb
LokhandeVidya <vidyalokhande635@gmail.com>
2025-07-02 12:43:06 +0530

    Add files via upload

---
+++ funjs.js
@@ -0,0 +1,11 @@
+function WelcomeMessage(){    //WelcomeMessage() = funname()
+    console.log("Welcome to MRECW");  // fun declaration
+}
+function add(a,b){
+    return a+b; // 10+20 
+}
+const mul = (a,b) => (a*b);
+console.log(mul(5,2)); //with parameters
+
+const greet =() => ("mul");
+console.log(greet());  //without parameters
---
+++ object.js
@@ -0,0 +1,5 @@
+let user = {name: "Vidya", dept: "CSE", age: 20};
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+console.log(user.dept);
